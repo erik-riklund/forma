7afed77e98cca6bd687e0dcdb032fb5594aa65d2
Erik R <erik.riklund@outlook.com>
2025-04-13 22:17:36 +0200

    loader: add hook and filesystem adapter

--- packages/loader/source/index.d.ts
+++ packages/loader/source/index.d.ts
@@ -1,1 +1,14 @@
-export {}
+/**
+ * Represents a template loader adapter.
+ */
+export interface ILoadAdapter
+{
+  /**
+   * Loads a template using the adapter.
+   * 
+   * @param identifier - The identifier of the template to load.
+   * @returns A promise that resolves to the loaded template as a string.
+   * @throws Will throw an error if the adapter fails to load the template.
+   */
+  load: (identifier: string) => Promise<string>;
+}
