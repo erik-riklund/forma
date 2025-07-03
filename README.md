f02c3d3373e0e517e321d292c4a037b0aa9be580
Erik R <erik.riklund@outlook.com>
2025-07-03 14:29:50 +0200

    reset project

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-<img src="https://raw.githubusercontent.com/erik-riklund/forma/b015d3d77f3c1c110b0ac12df2e036106866ccd9/logo.svg" width="64" height="64">
+<img src="images/logo.svg" width="64" height="64">
 
-# Forma
+## Forma - a component-based template engine
 
-> _Please note that this project is a work in progress_ 😃
+> _Please note that this project is a work in progress._
--- types.d.ts
+++ types.d.ts
@@ -1,24 +1,24 @@
-export { };
+export {};
 
 declare global
 {
   /**
-   * Represents a value that may be null.
-   */
-  type MaybeNull<T> = T | null;
-
-  /**
-   * Represents a value that may be void.
-   */
-  type MaybeVoid<T> = T | void;
-
-  /**
-   * Represents a value that may be undefined.
+   * Represents a value that could be `undefined`.
    */
   type Optional<T> = T | undefined;
 
   /**
-   * Represents a value that may be a promise.
+   * Represents a value that could be `null`.
+   */
+  type MaybeNull<T> = T | null;
+
+  /**
+   * Represents a value that could be a `Promise`.
    */
   type MaybePromise<T> = T | Promise<T>;
+
+  /**
+   * Represents a value that could be `void`.
+   */
+  type MaybeVoid<T> = T | void;
 }
